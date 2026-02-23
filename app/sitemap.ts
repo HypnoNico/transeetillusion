a78e737501d03a6ce41563a5a75e043aadb3efde
HypnoNico <contact@transeetillusion.fr>
2026-02-23 01:37:16 +0100

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://transeetillusion.fr",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://transeetillusion.fr/prestations",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://transeetillusion.fr/formations",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://transeetillusion.fr/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
